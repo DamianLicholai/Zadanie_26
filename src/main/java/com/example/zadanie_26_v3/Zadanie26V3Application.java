d3998d5443c92f2753ec30f3d02383e7ea6e0139
Damian <dlicholai@gmail.ocm>
2021-09-05 21:33:59 +0200

    Stworzenie projektu

---
+++ src/main/java/com/example/zadanie_26_v3/Zadanie26V3Application.java
@@ -0,0 +1,13 @@
+package com.example.zadanie_26_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie26V3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie26V3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanie_26_v3/Zadanie26V3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie_26_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie26V3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
